491f330fa9ef25af40c512227b5fb85d9eb57cdc
jkatz44 <jonkatz44@gmail.com>
2015-10-22 13:35:14 -0500

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,4 +9,4 @@
 card1 = Card.create([phone_number: '1234567890', business_name: 'Walgreens', points: 1000])
 card2 = Card.create([phone_number: '1234567890', business_name: 'CVS', points: 2000])
 card3 = Card.create([phone_number: '9876543210', business_name: 'Yogurtland', points: 3000])
-card4 = Card.create([phone_number: '9876543210', business_name: 'CVS', points: 500])
+card4 = Card.create([phone_number: '9876543210', business_name: 'Walmart', points: 500])
