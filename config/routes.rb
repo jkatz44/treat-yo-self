02d76ed564816ed91dca604f0012e8575f617a34
jkatz44 <jonkatz44@gmail.com>
2015-11-15 20:53:08 +0000

    Add link_to for fb login

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   patch 'business/do_add_card' => 'business#do_add_card'
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'logout', to: 'sessions#destroy'
-  get '/auth/facebook' => 'sessions#create'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
